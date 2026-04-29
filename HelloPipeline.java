fc8651ce0505c27a02e9475f31d745fbd7da2b66
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-04-29 13:03:51 +0530

    Update HelloPipeline.java

--- HelloPipeline.java
+++ HelloPipeline.java
@@ -1,5 +1,22 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloPipeline {
-    public static void main(String[] args) {
-        System.out.println("✅ Hello, CI/CD Pipeline is working!");
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from Jenkins Docker CI/CD Pipeline";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }
